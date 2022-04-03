1ddbc918556b8db355c68750d1a127e493524478
marcoigorr <petronio.mrc@gmail.com>
2022-04-03 20:18:05 +0200

    + frame rate + rectangle fill

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
@@ -8,12 +11,30 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D)this.getGraphics();
+    }
+
+    public void update(double dt) {
+        // System.out.println("" + dt + "s passed since last Frame");
+        // System.out.println(1 / dt + "fps");
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0,0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
     }
 
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
 
+            update(deltaTime);
+
+            try {
+                Thread.sleep(15);
+            } catch (Exception e) {
+                // Exception
+            }
         }
     }
-
 }
