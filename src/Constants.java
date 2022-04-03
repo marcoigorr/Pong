dbb3369a1af37faf97f5f2b1e91f2d54139ac80b
marcoigorr <petronio.mrc@gmail.com>
2022-04-03 21:22:07 +0200

    + Rectangle class + Constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,16 @@
+import java.awt.Color;
+
 public class Constants {
     public static final int SCREEN_WIDTH = 800;
     public static final int SCREEN_HEIGHT = 600;
     public static final String SCREEN_TITLE = "Pong";
+
+    public static final int PADDLE_WIDTH = 15;
+    public static final int PADDLE_HEIGHT = 90;
+    public static final int PADDLE_X_OFFSET = 20;
+    public static final int PADDLE_Y_OFFSET = SCREEN_HEIGHT/2 - (PADDLE_HEIGHT/2);
+
+    public static final int BALL_WIDTH = 10;
+
+    public static final Color PADDLE_COLOR = Color.WHITE;
 }
--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,14 @@
 import javax.swing.JFrame;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Window extends JFrame implements Runnable {
 
     Graphics2D g2;
+    KL keyListener = new KL();
+    Rect playerOne, ai, ball;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
@@ -11,7 +16,32 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
         g2 = (Graphics2D)this.getGraphics();
+
+        playerOne = new Rect(
+                Constants.PADDLE_X_OFFSET,
+                Constants.PADDLE_Y_OFFSET,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,
+                Color.WHITE
+        );
+
+        ai = new Rect(
+                Constants.SCREEN_WIDTH - (Constants.PADDLE_X_OFFSET + Constants.PADDLE_WIDTH),
+                Constants.PADDLE_Y_OFFSET,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,
+                Constants.PADDLE_COLOR
+        );
+
+        ball = new Rect(
+                Constants.SCREEN_WIDTH/2,
+                Constants.SCREEN_HEIGHT/2,
+                Constants.BALL_WIDTH,
+                Constants.BALL_WIDTH,
+                Constants.PADDLE_COLOR
+        );
     }
 
     public void update(double dt) {
@@ -19,6 +49,19 @@ public class Window extends JFrame implements Runnable {
         // System.out.println(1 / dt + "fps");
         g2.setColor(Color.BLACK);
         g2.fillRect(0,0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        playerOne.Draw(g2);
+        ai.Draw(g2);
+        ball.Draw(g2);
+
+        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("Sudo su");
+
+
+        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("sudo giú");
+
+        }
     }
 
     public void run() {
